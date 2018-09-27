f6ab1cd1df0930bb033e74c3623e812cf99a4333
Savannah Younts <savyounts@gmail.com>
2018-09-27 20:15:22 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,8 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select
+    link_to song.artist.name, song_artist_path(song, song.artist) if !song.artist.nil?
+  end
 end
