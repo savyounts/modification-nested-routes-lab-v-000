923954fa2537f8b3936e4cbc2915ff7d607065c1
Savannah Younts <savyounts@gmail.com>
2018-09-27 20:09:15 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
 
+  def artist_id_field(song)
+    if song.artist.nil?
+      f.collection_select :song_artist_id, Artist.all, :id, :name
+    else
+      f.hidden_field :aritst_id
+    end
+  end
 end
